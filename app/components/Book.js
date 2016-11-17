1bf31b69d1084031c6d2d1c90d701c21b5f15899
John <jenguyen@uci.edu>
2016-11-16 23:55:48 -0800

    Book component modifications

--- app/components/Book.js
+++ app/components/Book.js
@@ -1,29 +1,66 @@
 import React from 'react';
 import defaultThumbnail from 'img/default-thumbnail.jpg'
 
-class Book extends React.Component{
+export default class Book extends React.Component{
+
+  findISBNThirteen(ISBN) {
+    return ISBN.type === 'ISBN_13';
+  }
+
+  onThumbnailHover() {
+
+  }
+
   render(){
-    const {data} = this.props;
+    const { data } = this.props;
+    const title = data.volumeInfo.title;
+    const author = data.volumeInfo.authors
+        ? data.volumeInfo.authors[0]
+        : "Unknown";
+
+    const isbnID = data.volumeInfo.industryIdentifiers
+        ? data.volumeInfo.industryIdentifiers.find(this.findISBNThirteen).identifier
+        : "Unknown";
+    window.console.log(isbnID);
     return (
         <div className="row bookContainer">
-          <div className="col-md-3">
+          <div className="col-md-2">
             {data.volumeInfo.imageLinks ?
               <img src={data.volumeInfo.imageLinks.thumbnail}/>
               :
-              <img src={defaultThumbnail}></img>
+              <img onMouseOver={this.onThumbnailHover.bind(this)} src={defaultThumbnail}/>
             }
           </div>
-          <div className="col-md-6">
-            <div><strong>{data.volumeInfo.title}</strong></div>
-            <div><strong>{data.volumeInfo.authors[0]}</strong></div>
+          <div className="col-md-5">
+            <ul>
+              <li>
+                <label>Title: </label><strong>{title}</strong>
+              </li>
+              <li>
+                <label>Author: </label><strong>{author}</strong>
+              </li>
+              <li>
+                <label>ISBN: </label>{isbnID}
+              </li>
+            </ul>
           </div>
           <div className="col-md-3">
-              <a href="#" className="btn btn-primary">BUY</a>
-              <a href="#" className="btn btn-success">SELL</a>
+            <label>Buyers:</label><div></div>
+            <label>Sellers:</label><div></div>
+            <button className="bookDescriptionButton btn btn-primary">Description</button>
+          </div>
+          <div className="col-md-2">
+              <ul>
+                <li>
+                  <a href="#" className="barterButton btn btn-primary">BUY</a>
+                </li>
+                <li>
+                  <a href="#" className="barterButton btn btn-success">SELL</a>
+                </li>
+              </ul>
           </div>
         </div>
     );
   }
 }
 
-export default Book;
